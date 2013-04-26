f981366a6b1706cb46991aef00f6fa7f9dd38100
andy <anandan.rangasamy@escapemg.com>
2013-04-25 20:34:21 -0400

    Get all the unique foreign words per english word

--- h3/ibm_1.py
+++ h3/ibm_1.py
@@ -30,8 +30,22 @@ class IBM_model1(object):
         for es_line in self.es_lines:
             self.es_uniq_words.append(set(es_line.strip().split()))
 
+    def set_foreign_words(self):
+        k = 1
+        while k <= len(self.en_lines):
+            en_words_l = self.en_lines[k-1].strip().split();
+            for en_word in en_words_l:
+                if en_word in self.english_words:
+                    self.english_words[en_word].update(self.es_uniq_words[k-1])
+                else:
+                    self.english_words[en_word] = set(self.es_uniq_words[k-1])
+            k += 1
+
+
 if __name__ == "__main__":
     start = time.time()
     model = IBM_model1("corpus.en", "corpus.es")
-    print len(model.en_lines), len(model.es_lines), len(model.es_uniq_words)
+    model.set_foreign_words()
+    print len(model.en_lines), len(model.es_lines), len(model.es_uniq_words), \
+            len(model.english_words)
     print 'Elapsed time: ', time.time() - start, " seconds"
