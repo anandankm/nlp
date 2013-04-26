ae839e864f0510ae7cdf79924e08491e6e3e6420
andy <andy.compeer@gmail.com>
2013-04-26 16:31:28 -0400

    get uniq foriegn words for NULL alignment.

--- h3/ibm_1.py
+++ h3/ibm_1.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-
+# -*- coding: utf-8 -*-
 __author__="Anandan Rangasmay <andy.compeer@gmail.com>"
 __date__ ="$Apr 25, 2013"
 
@@ -14,6 +14,8 @@ class IBM_model1(object):
         self.set_en_filehandle()
         self.set_es_filehandle()
         self.es_uniq_words = []
+        self.es_uniq = set()
+        self.es_uniq_len = 0
         self.get_lines()
 
     ## Call this whenever a reset of the file handles is needed
@@ -28,12 +30,16 @@ class IBM_model1(object):
         self.en_lines = list(self.en_corpus_file)
         self.es_lines = list(self.es_corpus_file)
         for es_line in self.es_lines:
-            self.es_uniq_words.append(set(es_line.strip().split()))
+            line_set = set(es_line.strip().split())
+            self.es_uniq_words.append(line_set)
+            self.es_uniq.update(line_set)
+        self.es_uniq_len = len(self.es_uniq)
 
     def set_foreign_words(self):
         k = 1
+        self.english_words["NULL"] = self.es_uniq
         while k <= len(self.en_lines):
-            en_words_l = self.en_lines[k-1].strip().split();
+            en_words_l = self.en_lines[k-1].strip().split()
             for en_word in en_words_l:
                 if en_word in self.english_words:
                     self.english_words[en_word].update(self.es_uniq_words[k-1])
